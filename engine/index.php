951d56e150b7e3d36d791131c6f8620ed96b9ca6
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-10-21 20:13:09 +0300

    enable CORS

--- engine/index.php
+++ engine/index.php
@@ -1,4 +1,26 @@
 <?php
+// Set the allowed origin (change * to the specific domain you want to allow)
+header("Access-Control-Allow-Origin: *");
+
+// Set the allowed HTTP methods (e.g., GET, POST, PUT, DELETE)
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+// Set the allowed HTTP headers
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Allow credentials (if needed)
+header("Access-Control-Allow-Credentials: true");
+
+// Set the cache expiration time (in seconds) for the preflight request
+header("Access-Control-Max-Age: 3600");
+
+// Handle preflight requests
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    // This is a preflight request, so just respond with the allowed headers
+    header("HTTP/1.1 200 OK");
+    exit;
+}
+
 $rawDomain = isset($_POST['domain']) ? $_POST['domain'] : "https:\/\/peviitor.ro";
 $rawDomain = isset($_GET['domain']) ? $_GET['domain'] : $rawDomain;
 
